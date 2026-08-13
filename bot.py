7949256c85e77c67672122751f43495b3630d958
weekey13 <weekweekey13@gmail.com>
2026-08-13 22:52:34 +0300

    Hide Telegram token from httpx logs

--- bot.py
+++ bot.py
@@ -65,6 +65,8 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s %(levelname)s %(name)s: %(message)s",
 )
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("httpcore").setLevel(logging.WARNING)
 
 BOT_TOKEN = ""
 ADMIN_USERNAMES: set[str] = set()
